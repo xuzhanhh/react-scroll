008c896c76ca601427a3374e9b117b0a3597191a
xuzhanhh <xuzhanhh@gmail.com>
2018-07-24 10:30:06 +0800

    test username

--- src/ScrollBar.js
+++ src/ScrollBar.js
@@ -138,6 +138,7 @@ export default class ScrollBar extends Component {
                 } onMouseDown={
                     (event) => {
                         //test
+                        //test2
                         event.preventDefault();
                         const { target, clientY } = event;
                         const { top: targetTop } = target.getBoundingClientRect();
